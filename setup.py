bdf7f832aa86e7116ce4b9e6f19d2c76cd219d79
Johannes Baiter <johannes.baiter@gmail.com>
2016-04-26 15:54:11 +0200

    Version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zotero-cli",
-    version="0.1",
+    version="0.2",
     description="Command-line interface for the Zotero API",
     author="Johannes Baiter",
     author_email="johannes.baiter@gmail.com",
